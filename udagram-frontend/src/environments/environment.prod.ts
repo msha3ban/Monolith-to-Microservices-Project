75b10ec2ad1060754254074effba9fcd295d9497
msha3ban <mostafa.a.shaban@gmail.com>
2023-03-15 23:40:29 +0200

    add aws credentials

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a092d791d9df24be69a98c906bed3e16-1173355211.eu-central-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a8b22716e7ba44419abccd1eecf3c1ce-1101111889.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
