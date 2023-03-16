bf086298bc99dd5e92f57a4e4978b6b05396f99a
msha3ban <mostafa.a.shaban@gmail.com>
2023-03-16 17:55:21 +0200

    update k8s rescources

--- mydeployment.sh
+++ mydeployment.sh
@@ -1,11 +1,19 @@
-# kubectl delete deployment udagram-api-feed
-# kubectl delete deployment udagram-api-user
-# kubectl delete deployment udagram-frontend
-# kubectl delete deployment udagram-reverseproxy
-# kubectl apply -f ./udagram-api-feed/deployment.yaml
-# kubectl apply -f ./udagram-api-user/deployment.yaml
-# kubectl apply -f ./udagram-frontend/deployment.yaml
-# kubectl apply -f ./udagram-reverseproxy/deployment.yaml
+# kubectl delete cm env-configmap
+# kubectl delete cm aws-secret
+# kubectl delete secrets env-secret
+
+# kubectl apply -f aws-secret.yaml
+# kubectl apply -f env-secret.yaml
+# kubectl apply -f env-configmap.yaml
+
+kubectl delete deployment udagram-api-feed
+kubectl delete deployment udagram-api-user
+kubectl delete deployment udagram-frontend
+kubectl delete deployment udagram-reverseproxy
+kubectl apply -f ./udagram-api-feed/deployment.yaml
+kubectl apply -f ./udagram-api-user/deployment.yaml
+kubectl apply -f ./udagram-frontend/deployment.yaml
+kubectl apply -f ./udagram-reverseproxy/deployment.yaml
 
 
 # kubectl delete svc udagram-frontend
@@ -18,28 +26,22 @@
 # kubectl delete svc publicreverseproxy
 
 
-kubectl apply -f ./udagram-api-feed/service.yaml
-kubectl apply -f ./udagram-api-user/service.yaml
-kubectl apply -f ./udagram-frontend/service.yaml
-kubectl apply -f ./udagram-reverseproxy/service.yaml
+# kubectl apply -f ./udagram-api-feed/service.yaml
+# kubectl apply -f ./udagram-api-user/service.yaml
+# kubectl apply -f ./udagram-frontend/service.yaml
+# kubectl apply -f ./udagram-reverseproxy/service.yaml
 
-#kubectl get nodes
-#kubectl get pods
-#kubectl get deployments
-#kubectl get services
-#kubectl expose deployment udagram-frontend --type=LoadBalancer --name=publicfrontend
-#kubectl expose deployment udagram-reverseproxy --type=LoadBalancer --name=publicreverseproxy
-#./mydeployment.sh
+# kubectl get nodes
+# kubectl get pods
+# kubectl get deployments
+# kubectl get services
+# kubectl expose deployment udagram-frontend --type=LoadBalancer --name=publicfrontend
+# kubectl expose deployment udagram-reverseproxy --type=LoadBalancer --name=publicreverseproxy
+# ./mydeployment.sh
 
-#aws eks update-kubeconfig --region us-east-1 --name myUdagramEKS
-
-# kubectl delete cm env-configmap.yaml
-# kubectl delete secrets aws-secret.yaml
-# kubectl delete secrets env-secret.yaml
-
-# kubectl apply -f aws-secret.yaml
-# kubectl apply -f env-secret.yaml
-# kubectl apply -f env-configmap.yaml
+# aws eks update-kubeconfig --region us-east-1 --name myUdagramEKS
 
 # kubectl get configmap
 # kubectl get secrets
+
+# kubectl set image deployment udagram-frontend udagram-frontend=sha3baan/udagram-frontend:v6
